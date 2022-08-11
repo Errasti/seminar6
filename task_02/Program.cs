d0a776d898d913c7d7ae578db1b07feaef2aeef6
errasti <errasti007@yandex.ru>
2022-08-11 22:38:21 +0300

    Initial commit

---
+++ task_02/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {1,2,3,4,5};
+int tmp = 0;
+for (int i = 0, j = array.Length - 1; i < array.Length / 2; i++,j--)
+{
+    tmp = array[i];
+    array[i] = array[j];
+    array[j] = tmp;
+}
+for ( int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+
