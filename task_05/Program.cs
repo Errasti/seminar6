fcc93d6530eef40e71633501ae9f198f8a99a243
errasti <errasti007@yandex.ru>
2022-08-12 01:08:35 +0300

    Initial commit

---
+++ task_05/Program.cs
@@ -0,0 +1,16 @@
+﻿for ( int i = 1; i < 101; i++)
+{
+    if ( i % 3 == 0 )
+    {
+        Console.Write("Fizz ");
+    }
+    else if ( i % 5 == 0 )
+    {
+        Console.Write("Buzz ");
+    }
+    else if ( i % 5 == 0 && i % 3 == 0 )
+    {
+        Console.Write("FizzBuzz ");
+    }
+    Console.Write(i + " ");
+}
