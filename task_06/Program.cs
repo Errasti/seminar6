a1c6d93b946f6b281b782f75b2be25ca76a3490c
errasti <errasti007@yandex.ru>
2022-08-12 01:44:54 +0300

    Initial commit

---
+++ task_06/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число в десятичной системе: ");
+int n = int.Parse(Console.ReadLine());
+int[] res = new int[32];
+int i = 0;
+while( n > 0 )
+{
+    res[i] = n % 2;
+    i++;
+    n /= 2;
+}
+
+for ( int j = i - 1; j >= 0; j-- )
+{
+    Console.Write(res[j]);
+}
+
