92ae30dc66096f0e22cc1e506feddca9e841fe4d
errasti <errasti007@yandex.ru>
2022-08-12 00:48:47 +0300

    Initial commit

---
+++ task_04/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {1,2,3,4,5};
+int[] copyArray = GetCopy(array);
+
+Console.WriteLine("\n Копия массива: {0} ", String.Join(" ", GetCopy(array)));
+Console.WriteLine();
+
+int[] GetCopy(int[] array)
+{
+    int[] copy = new int[array.Length];
+
+    for (int i = 0; i < array.Length; i++ )
+    {
+        copy[i] = array[i];
+    }
+    
+    return copy;
+}
+
