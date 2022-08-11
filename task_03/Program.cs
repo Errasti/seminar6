1c05f0646e699bd7e9202dabd47088270d43fcbb
errasti <errasti007@yandex.ru>
2022-08-12 00:33:15 +0300

    Initial commit

---
+++ task_03/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Введите первое число: ");
+int firstNum = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secNum = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int thirdNum = int.Parse(Console.ReadLine());
+
+/* if ( firstNum + secNum > thirdNum && firstNum + thirdNum > secNum && secNum + thirdNum > firstNum)
+{
+    Console.WriteLine("Треугольник с такими сторонами возможен");
+}
+else
+{
+    Console.WriteLine("Треугольник с такими сторонами невозможен");
+}
+*/
+
+bool IsTriangle (int a, int b, int c)
+{
+    if ( a + b > c && c + b > a && a + c > b)
+    {
+       return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+if (IsTriangle(firstNum,secNum,thirdNum))
+{
+    Console.WriteLine("Треугольник реален");
+}
+else
+{
+    Console.WriteLine("Треугольник нереален");
+}
