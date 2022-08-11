58458ba58e231ae38ba6e40dfd75cf4fb551952b
errasti <errasti007@yandex.ru>
2022-08-12 01:11:40 +0300

    Edited

--- task_05/Program.cs
+++ task_05/Program.cs
@@ -1,16 +1,16 @@
 ﻿for ( int i = 1; i < 101; i++)
 {
-    if ( i % 3 == 0 )
+    if (i % 5 == 0 && i % 3 == 0 )
     {
-        Console.Write("Fizz ");
+        Console.Write("FizzBuzz ");
     }
     else if ( i % 5 == 0 )
     {
         Console.Write("Buzz ");
     }
-    else if ( i % 5 == 0 && i % 3 == 0 )
+    else if ( i % 3 == 0 )
     {
-        Console.Write("FizzBuzz ");
+        Console.Write("Fizz ");
     }
     Console.Write(i + " ");
 }
